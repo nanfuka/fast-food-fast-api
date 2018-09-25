a1437328fb9e8f13ce1c15d58692d49bbb8e3ee1
nanfuka <kalungi2k6@gmail.com>
2018-09-25 18:47:07 +0300

    add methods[#160750713]

---
+++ api/model/data.py
@@ -0,0 +1,121 @@
+import jwt
+import datetime
+from flask import jsonify
+from api.model.Responses import auth_fail
+from functools import wraps
+from flask import request
+
+
+class DataStore:
+
+    def __init__(self, users=[], orders=[]):
+
+        self.users = users
+
+        self.orders = orders
+        self.key = "my Jesus I love thee"
+
+    def createUser(self, user):
+        """
+        function to create a new user and append new user to the list of users
+        """
+        self.users.append(user)
+        return user
+
+    def addOrders(self, req):
+        """
+        function for placing a new order.
+        this appends the newly placed order to the list of orders
+        """
+        self.orders.append(req)
+        return req
+
+    def getAllOrders(self):
+        """
+        function to return all orders
+        """
+        return self.orders
+
+    def getAllOrdersForUser(self, order):
+        """
+        function to return all orders for a specific user
+        """
+        response = []
+        for req in self.orders:
+            if req.getOwner() == order:
+                response.append(req.getDictionary())
+        return response
+
+    def getASpecificRequestsForUser(self, requestId):
+        """
+        function to get a specific order for a specific user
+        """
+        for req in self.orders:
+            if req.getOrderId() == requestId:
+                return req.getDictionary()
+        return None
+
+    def getASpecificOrderForUser(self, requestId):
+        for req in self.orders:
+            if req.getOrderId() == requestId:
+                return req.getDictionary()
+        return None
+
+    def updateOrder(self, order):
+        """
+        function to update an order
+        """
+        i = 0
+        for req in self.orders:
+            if req.getOrderId() == order.getOrderId():
+                self.orders[i] = order
+                return order.getDictionary()
+            i = i+1
+        return None
+
+    def searchList(self, username):
+        """
+        function to search a list of users for a specific username
+        """
+        for item in self.users:
+            if item.getUserName() == username:
+                return item
+            else:
+                return None
+
+    def generate_auth_token(self, user):
+        """
+        function to generate an auth-token
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow(
+
+                ) + datetime.timedelta(minutes=2000),
+                'iat': datetime.datetime.utcnow(),
+                'user': user
+            }
+            return jwt.encode(
+                payload,
+                self.key,
+                algorithm='HS256'
+            ).decode('utf-8')
+        except Exception as e:
+            return e
+
+    def token_required(self, func):
+
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            token = None
+            if "Authorization" in request.headers:
+                token = request.headers['Authorization']
+            else:
+                return jsonify(auth_fail), 401
+            try:
+                data = jwt.decode(token, self.key)
+                current_user = self.searchList(data['user']['username'])
+            except:
+                return jsonify(auth_fail), 401
+            return func(current_user, *args, **kwargs)
+        return decorated
---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, jsonify, request
+from api.model.Responses import registration_successful, \
+    login_fail, request_fail, create_request_successful, create_request_fail
+from api.model.User import User
+from api.model.orderRequest import OrderRequest
+from api.model.data import DataStore
+
+
+app = Flask(__name__)
+
+temp_orders = [OrderRequest("beef", "fresh", "36", "Deb"),  OrderRequest(
+    "bacon", "fresh", "3", "Deb")]
+temp_users = [User("Nsubuga", "Kalungiowak",
+                   "llkldf@gmail.com", "Deb", "boosiko", True)]
+data_store = DataStore(temp_users, temp_orders)
+
+
+@app.route('/')
+def api_documentation():
+    return "WELCOME TO FAST FOOD FAST APPLICATION"
+
+
+@app.route('/api/v1/login', methods=['POST'])
+def login():
+    """
+    login
+    """
+
+    data = request.get_json(force=True)
+    username = data.get('username', None)
+    password = data.get('password', None)
+
+    user = data_store.searchList(username)
+    if user is not None:
+        if user.verify_password(password):
+            response = user.getDictionary()
+            response["token"] = data_store.generate_auth_token(response)
+            response["success"] = True
+            print(str(response))
+            return jsonify(response)
+        else:
+            return jsonify(login_fail), 200
+    else:
+        return jsonify(login_fail), 200
+
+# signup user
+
+
+@app.route('/api/v1/register', methods=['POST'])
+def register_user():
+    data = request.args
+    firstName = data.get("first_name")
+    lastName = data.get("last_name")
+    email = data.get("email")
+    username = data.get("username")
+    password = data.get("password")
+    if firstName is not None and lastName is not None and \
+            email is not None and username is not None and \
+            password is not None:
+        user = data_store.searchList(username)
+        if user is None:
+            response = data_store.createUser(
+                User(
+                    firstName, lastName, email, username, password)).\
+                    getDictionary()
+            response["token"] = data_store.generate_auth_token(response)
+            registration_successful["user"] = response
+            return jsonify(registration_successful)
+
+        else:
+            return jsonify(login_fail), 401
+    else:
+        return jsonify(create_request_fail)
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
