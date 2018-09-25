2bea4eaf0cb2a02d257ad6607e24d254843400d5
nanfuka <kalungi2k6@gmail.com>
2018-09-25 21:47:48 +0300

    add tests [delivers #160752464]

---
+++ api/tests/Basetest.py
@@ -0,0 +1,55 @@
+from unittest import TestCase
+from flask import json
+from app import app
+from api.model.User import User
+import unittest
+
+
+
+class BaseTest(TestCase):
+
+
+    def setUp(self):
+        """
+        initialise the app module
+        create a pytest fixture called client() that configures the application for testing
+        """
+        self.app = app
+        self.context = self.app.app_context()
+        self.context.push()
+        self.client = self.app.test_client()
+  
+   
+    def tearDown(self):
+        """
+        Release Context
+        """
+        self.context.pop()
+
+    
+    def get_auth_token(self):
+        """
+        create token from login
+        """
+        response = self.client.post('/api/v1/login',content_type='application/json', data=json.dumps(dict(username='Deb', password='boosiko')))
+        reply = json.loads(response.data.decode())
+        self.assertEquals(reply['success'],True)
+        if reply['success']:
+            return reply['token']
+        else:
+            return None
+
+    
+    def get_request_id(self):
+        """
+        Get Request Id for Test Purposes
+        """
+        head={'Authorization':self.get_auth_token(),'content_type':'application/json'}
+        
+        request={'foodorder':'bacon', 'description':'fresh', 'quantity':'3'}
+        response = self.client.post('/api/v1/orders',headers=head,data=json.dumps(request))
+        reply = json.loads(response.data.decode())
+        assert "200 OK" ==response.status
+        if reply['success']:
+            return reply['data']['id']
+
