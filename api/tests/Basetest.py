1ce11ede5f037e17fc9aaa6973b00b0da7dc46b3
nanfuka <kalungi2k6@gmail.com>
2018-09-27 05:19:41 +0300

    refactor code [delivers#160797482]

--- api/tests/Basetest.py
+++ api/tests/Basetest.py
@@ -5,51 +5,46 @@ from api.model.User import User
 import unittest
 
 
-
 class BaseTest(TestCase):
-
 
     def setUp(self):
         """
         initialise the app module
-        create a pytest fixture called client() that configures the application for testing
+        create a pytest fixture called client() 
+        that configures the application for testing
         """
         self.app = app
         self.context = self.app.app_context()
         self.context.push()
         self.client = self.app.test_client()
-  
-   
+
     def tearDown(self):
-        """
-        Release Context
-        """
+        """Release Context"""
         self.context.pop()
 
-    
     def get_auth_token(self):
-        """
-        create token from login
-        """
-        response = self.client.post('/api/v1/login',content_type='application/json', data=json.dumps(dict(username='Deb', password='boosiko')))
+        """create token from login"""
+        response = self.client.post('/api/v1/login',
+                                    content_type='application/json',
+                                    data=json.dumps(dict(username='Deb',
+                                                         password='boosiko')))
         reply = json.loads(response.data.decode())
-        self.assertEquals(reply['success'],True)
+        self.assertEquals(reply['success'], True)
         if reply['success']:
             return reply['token']
         else:
             return None
 
-    
     def get_request_id(self):
-        """
-        Get Request Id for Test Purposes
-        """
-        head={'Authorization':self.get_auth_token(),'content_type':'application/json'}
-        
-        request={'foodorder':'bacon', 'description':'fresh', 'quantity':'3'}
-        response = self.client.post('/api/v1/orders',headers=head,data=json.dumps(request))
+        """Get Request Id for Test Purposes"""
+        head = {'Authorization': self.get_auth_token(
+        ), 'content_type': 'application/json'}
+
+        request = {'food_order': 'bacon',
+                   'description': 'fresh', 'quantity': '3'}
+        response = self.client.post(
+            '/api/v1/orders', headers=head, data=json.dumps(request))
         reply = json.loads(response.data.decode())
-        assert "200 OK" ==response.status
+        assert "200 OK" == response.status
         if reply['success']:
             return reply['data']['id']
-
--- api/tests/test_TestCreateRequest.py
+++ api/tests/test_TestCreateRequest.py
@@ -5,38 +5,44 @@ import json
 from api.tests.Basetest import BaseTest
 
 
-
 class TestCreateRequests(BaseTest):
 
-    # Check if URL path exists and is protected
-	def test_if_URL_exists(self):
-		response = self.client.post('/api/v1/orders' )
-		assert "401 UNAUTHORIZED" ==response.status
+    def test_if_URL_exists(self):
+        """ Check if URL path exists and is protected"""
+        response = self.client.post('/api/v1/orders')
+        assert "401 UNAUTHORIZED" == response.status
 
-	#Test for non authenticated user
-	def test_api_check_non_authorised_user(self):
-		with self.client:
-			response = self.client.post('/api/v1/orders')
-			reply = json.loads(response.data.decode())
-			self.assertEquals(reply["success"],False)
-			self.assertEquals(reply["message"],"You are not authorised to access this page.")
+    def test_api_check_non_authorised_user(self):
+        """"Test for non authenticated user"""
+        with self.client:
+            response = self.client.post('/api/v1/orders')
+            reply = json.loads(response.data.decode())
+            self.assertEquals(reply["success"], False)
+            self.assertEquals(
+                reply["message"],
+                "You are not authorised to access this page.")
 
-	# Test for authenticated user but no parameters
-	def test_api_when_no_parameters_have_been_passed(self):
-		with self.client:
-			head={'Authorization':self.get_auth_token()}
-			response = self.client.post('/api/v1/orders',headers = head,data=json.dumps({}) )
-			reply = json.loads(response.data.decode())
-			self.assertEquals(reply['success'],False)
-			self.assertEquals(reply['message'],'All fields required.')
+    def test_api_when_no_parameters_have_been_passed(self):
+        """Test for authenticated user but no parameters"""
+        with self.client:
+            head = {'Authorization': self.get_auth_token()}
+            response = self.client.post(
+                '/api/v1/orders', headers=head, data=json.dumps({}))
+            reply = json.loads(response.data.decode())
+            self.assertEquals(reply['success'], False)
+            self.assertEquals(reply['message'], 'All fields required.')
 
-	# Test for authenticated and parameters provided
-	def test_api_when_parameters_have_been_passed(self):
-		with self.client:
-			head={'Authorization':self.get_auth_token(),'content_type':'application/json'}
-			request={'foodorder':'bacon', 'description':'fresh', 'quantity':'3'}
-			response = self.client.post('/api/v1/orders',headers=head,data=json.dumps(request))
-			reply = json.loads(response.data.decode())
-			assert "200 OK" ==response.status
-			self.assertEquals(reply['success'],True)
-			self.assertEquals(reply['message'],'Your request was submitted successfully.')
+    def test_api_when_parameters_have_been_passed(self):
+        """Test for authenticated and parameters provided"""
+        with self.client:
+            head = {'Authorization': self.get_auth_token(
+            ), 'content_type': 'application/json'}
+            request = {'food_order': 'bacon',
+                       'description': 'fresh', 'quantity': '3'}
+            response = self.client.post(
+                '/api/v1/orders', headers=head, data=json.dumps(request))
+            reply = json.loads(response.data.decode())
+            assert "200 OK" == response.status
+            self.assertEquals(reply['success'], True)
+            self.assertEquals(reply['message'],
+                              'Your request was submitted successfully.')
